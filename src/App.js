8e8a0ed016b4e81430449a7426b7156df2895814
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-04-03 16:16:35 +0200

    Solving part 1 of the recap-project-4_theme-creator-app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,24 @@
 import "./App.css";
 import "./db";
 import Header from "./components/Header/Header";
-import Card from "./components/ColorCard/ColorCard";
+import ColorCard from "./components/ColorCard/ColorCard";
+import { themes } from "./db";
 
 function App() {
+  const theme = themes[0];
   return (
     <div className="app">
       <Header />
       <main className="app__main">
-        <Card />
+        <h2 className="theme-title">{theme.name}</h2>
+        <ul className="color-list">
+          {theme.colors.map((color) => (
+            <li key={color.role}>
+              <ColorCard color={color} />
+            </li>
+          ))}
+        </ul>
       </main>
-      {/* <Footer /> */}
     </div>
   );
 }
--- src/components/ColorCard/ColorCard.js
+++ src/components/ColorCard/ColorCard.js
@@ -1,6 +1,16 @@
 import "./colorCard.css";
-import { themes } from "../../db";
 
-export default function Card() {
-  return;
+export default function ColorCard({ color }) {
+  return (
+    <article className="color-card">
+      <div
+        className="color-card__display"
+        style={{ backgroundColor: color.value }}
+      ></div>
+      <div className="color-card__info">
+        <span className="color-card__role">{color.role}</span>
+        <span className="color-card__hex">{color.value}</span>
+      </div>
+    </article>
+  );
 }
