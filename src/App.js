2e597597e73d0de16c0e660dda2f07db51cdb3c5
Jessica Windoffer <jessica.windoffer@t-online.de>
2024-04-05 16:12:20 +0200

    Finishing Part 3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import "./App.css";
 import "./db";
 import Header from "./components/Header/Header";
-import { themes } from "./db";
+import { themes as initialThemes } from "./db";
 import Theme from "./components/Theme/Theme";
+import ThemeForm from "./components/ThemeForm/ThemeForm";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
 
 function App() {
+  const [themes, setThemes] = useState(initialThemes);
+
+  function handleAddTheme(newTheme) {
+    const newThemeWithId = { ...newTheme, id: uuid() };
+    setThemes([newThemeWithId, ...themes]);
+  }
+
   return (
     <div className="app">
       <Header />
       <main className="app__main">
         {" "}
+        <ThemeForm onSubmit={handleAddTheme} />
         <ul className="theme-list">
           {themes.map((theme) => (
             <li key={theme.id}>
